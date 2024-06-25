2ee0f91c99aebcce6c37562cbcbd1a59d35c5c4b
Filip Rachůnek <Filip.Rachunek@gmail.com>
2024-06-25 14:51:26 +0200

    Screenshot.

--- main.gd
+++ main.gd
@@ -20,12 +20,23 @@ func _process(delta):
 func _input(_event):
 	if Input.is_action_just_pressed("pause_game"):
 		pause_game()
+	if Input.is_action_just_pressed("take_screenshot"):
+		take_screenshot()
 
 
 func _notification(what):
 	if what == MainLoop.NOTIFICATION_APPLICATION_FOCUS_OUT:
 		pause_game()
 
+
 func pause_game():
 	pause_label.visible = true
 	get_tree().paused = true
+
+
+func take_screenshot():
+	var timestamp = Time.get_datetime_string_from_system(false, true).replace(":", "-")
+	await RenderingServer.frame_post_draw
+	var sshot = get_viewport().get_texture().get_image()
+	# sshot.resize(200, 100, Image.INTERPOLATE_TRILINEAR)
+	sshot.save_png("user://screenshot " + timestamp + ".png")
